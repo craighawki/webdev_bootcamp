f8842ed78052cea7c6500faeada66df767c26aa4
Craig Hawkins <craighawki@gmail.com>
2023-07-19 22:25:25 -0700

    Conditionals

---
+++ Conditionals/app.js
@@ -0,0 +1,11 @@
+
+let random = Math.random();
+if (random < 0.5) {
+    console.log("Your number is less than 0.5!");
+    console.log(random);
+}
+
+if (random >= 0.5) {
+    console.log("Your number GT 0.5!");
+    console.log(random);
+}
