ebbb3d8a31b2f7f1fd4cf250bcb0a5e445fcd91f
Craig Hawkins <craighawki@gmail.com>
2023-07-23 00:49:13 -0700

    More comditionals

--- Conditionals/app.js
+++ Conditionals/app.js
@@ -1,11 +1,32 @@
 
-let random = Math.random();
-if (random < 0.5) {
-    console.log("Your number is less than 0.5!");
-    console.log(random);
-}
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Your number is less than 0.5!");
+//     console.log(random);
+// }
 
-if (random >= 0.5) {
-    console.log("Your number GT 0.5!");
-    console.log(random);
+// if (random >= 0.5) {
+//     console.log("Your number GT 0.5!");
+//     console.log(random);
+// }
+
+// const dayOfWeek = 'Friday';
+
+// if (dayOfWeek === 'Monday') {
+//     console.log("UGHHH THE DUDE HATES MONDAYS!")
+// } else if (dayOfWeek === 'Saturday') {
+//     console.log("YAY PEOPLE TEND TO REALLY LIKE SATURDAYS!")
+// } else if (dayOfWeek === 'Friday') {
+//     console.log("Fridays Are Decent")
+// }
+
+
+const age = 30;
+
+if (age < 5) {
+    console.log("You're a baby,so you can get in for free!")
+} else if (age < 10) {
+    console.log("You are a child. You pay $10.00")
+} else if (age < 65) {
+    console.log("You are an adult, so you pay $20.00")
 }
