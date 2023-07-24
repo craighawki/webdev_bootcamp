8b82908fa419e4a22b01347233ed955c0bde23bb
Craig Hawkins <craighawki@gmail.com>
2023-07-23 22:00:53 -0700

    More conditionals

--- Conditionals/app.js
+++ Conditionals/app.js
@@ -2,26 +2,27 @@
 // let random = Math.random();
 // if (random < 0.5) {
 //     console.log("Your number is less than 0.5!");
-//     console.log(random);
-// }
-
-// if (random >= 0.5) {
+// } else {
 //     console.log("Your number GT 0.5!");
-//     console.log(random);
 // }
+// console.log(random);
 
-// const dayOfWeek = 'Friday';
+// const dayOfWeek = prompt('Enter a day').toLowerCase();
 
-// if (dayOfWeek === 'Monday') {
+// if (dayOfWeek === 'monday') {
 //     console.log("UGHHH THE DUDE HATES MONDAYS!")
-// } else if (dayOfWeek === 'Saturday') {
+// } else if (dayOfWeek === 'saturday') {
 //     console.log("YAY PEOPLE TEND TO REALLY LIKE SATURDAYS!")
-// } else if (dayOfWeek === 'Friday') {
+// } else if (dayOfWeek === 'friday') {
 //     console.log("Fridays Are Decent")
+// } else if (dayOfWeek === 'tuesday') {
+//     console.log("MEH")
+// } else {
+//     console.log("MEH")
 // }
 
 
-const age = 30;
+const age = 65;
 
 if (age < 5) {
     console.log("You're a baby,so you can get in for free!")
@@ -29,4 +30,6 @@ if (age < 5) {
     console.log("You are a child. You pay $10.00")
 } else if (age < 65) {
     console.log("You are an adult, so you pay $20.00")
+} else {
+    console.log("You are a senior, so you pay $10.00")
 }
