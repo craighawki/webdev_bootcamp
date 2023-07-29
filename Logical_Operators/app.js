7d8d6754af516481c53c28465fc305eb3836803b
Craig Hawkins <craighawki@gmail.com>
2023-07-29 00:06:59 -0700

    stuff

---
+++ Logical_Operators/app.js
@@ -0,0 +1,12 @@
+// const password = prompt("Enter your password");
+// if (password.length >= 6 && password.indexOf(' ') === -1) {
+//     console.log("VALID PASSWORD!")
+// } else {
+//     console.log("password not accepted! Fix that shit!")
+// }
+
+// 0-5 free
+// 5-10 $10
+// 10-65 $20
+//65+ free
+
