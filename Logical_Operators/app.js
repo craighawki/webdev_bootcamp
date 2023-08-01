6c1ec0c878190bf7bbdb5fac50e07d727eecba75
Craig Hawkins <craighawki@gmail.com>
2023-07-31 21:31:35 -0700

    Switch statement

--- Logical_Operators/app.js
+++ Logical_Operators/app.js
@@ -10,3 +10,23 @@
 // 10-65 $20
 //65+ free
 
+// const age = 90;
+// if (age >= 0 && age < 5 || age >= 65) {
+//     console.log("Free");
+// } else if (age >= 5 && age < 10) {
+//     console.log("$10");
+// } else if (age >= 10 && age < 65) {
+//     console.log("$20")
+// } else {
+//     console.log("INVALID AGE!")
+// }
+
+// const firstName = prompt("Enter your first name: ");
+// if (!firstName) {
+//     firstName = prompt("TRY AGAIN!!!");
+// }
+
+const age = 45;
+if (!(age >= 0 && age < 5 || age >= 65)) {
+    console.log("YOU ARE  NOT A BABY OR A SENIOR!");
+}
