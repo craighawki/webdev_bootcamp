371cf39d649d5cbda892e642a8b85af0673a948b
craighawkins <craighawki@gmail.com>
2023-10-24 21:53:47 -0700

    practice functions

---
+++ MyToDos/todos.js
@@ -0,0 +1,11 @@
+let input = prompt('what would you like to do?');
+const todos = ['Collect Chicken Eggs', 'Clean Litter Box'];
+while (input !== 'quit' && input !== 'q') {
+    if (input === 'list') {
+        console.log('*********************')
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('*********************')
+    }
+}
