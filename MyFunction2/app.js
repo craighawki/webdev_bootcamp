e7c6568575d121ef111f4254c4427426e8348470
Craig Hawkins <craighawki@gmail.com>
2023-10-31 17:35:45 -0700

    scope

--- MyFunction2/app.js
+++ MyFunction2/app.js
@@ -1,7 +1,16 @@
-function collectEggs() {
-    let totalEggs = 6;
-    console.log(totalEggs);
+
+// let totalEggs = 0;
+// function collectEggs() {
+//     totalEggs = 6;
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
+
+let bird = 'Scarlet Macaw';
+function birdWatch() {
+    // let bird = 'Great Blue Heron';
+    console.log(bird);
 }
 
-collectEggs();
-console.log(totalEggs);
+birdWatch();
