a5bc448e53b2bbb7d0a3962864767d7fdd6618f8
Craig Hawkins <craighawki@gmail.com>
2023-11-01 22:17:00 -0700

    more scpoe

--- MyFunction2/app.js
+++ MyFunction2/app.js
@@ -7,10 +7,16 @@
 // collectEggs();
 // console.log(totalEggs);
 
-let bird = 'Scarlet Macaw';
-function birdWatch() {
-    // let bird = 'Great Blue Heron';
-    console.log(bird);
-}
+// let bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     // let bird = 'Great Blue Heron';
+//     console.log(bird);
+// }
 
-birdWatch();
+// birdWatch();
+
+let radius = 8;
+if (radius > 0) {
+    const PI = 3.14159;
+    let msg = 'HIII!'
+}
