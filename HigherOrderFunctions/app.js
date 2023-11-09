53dabd14f62d8dc663c87b91ab3e6e33887416f6
Craig Hawkins <craighawki@gmail.com>
2023-11-08 21:03:03 -0800

    functions in functions

--- HigherOrderFunctions/app.js
+++ HigherOrderFunctions/app.js
@@ -1,17 +1,38 @@
-function callTwice(func) {
-    func();
-    func();
-}
+// function callTwice(func) {
+//     func();
+//     func();
+// }
 
-function callTenTimes(f) {
-    for (let i = 0; i < 10; i++) {
-        f();
-    }
-}
+// function callTenTimes(f) {
+//     for (let i = 0; i < 10; i++) {
+//         f();
+//     }
+// }
 
-function rollDie() {
-    const roll = Math.floor(Math.random() * 6) + 1;
-    console.log(roll);
-}
+// function rollDie() {
+//     const roll = Math.floor(Math.random() * 6) + 1;
+//     console.log(roll);
+// }
 
-callTwice(rollDie)
+// callTwice(rollDie)
+
+// function makeMysteryFunc() {
+//     const rand = Math.random();
+//     if (rand > 0.5) {
+//         return function () {
+//             console.log("CONGRATS, I AM A GOOD FUNCTION!");
+//             console.log("YOU WIN A MILLION DOLLARS");
+//         }
+//     } else {
+//         return function () {
+//             alert("YOU HAVE BEEN INFECTED BY A COMPUTER VIRUS!")
+//             alert("STOP TRYING TO CLOSE THIS WINDOW")
+//             alert("STOP TRYING TO CLOSE THIS WINDOW")
+//             alert("STOP TRYING TO CLOSE THIS WINDOW")
+//             alert("STOP TRYING TO CLOSE THIS WINDOW")
+//             alert("STOP TRYING TO CLOSE THIS WINDOW")
+//         }
+//     }
+// }
+
+
