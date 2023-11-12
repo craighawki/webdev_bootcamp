cffeced7ebc38b274fbb80f2dbff0b5d1b68a685
craighawkins <craighawki@gmail.com>
2023-11-11 23:56:18 -0800

    Defining methods

--- MyFunction2/app.js
+++ MyFunction2/app.js
@@ -2,7 +2,16 @@
 //     return x + y;
 // }
 
-const add = function (x, y) {
-    return x + y;
-}
+// const add = function (x, y) {
+//     return x + y;
+// }
 
+const myMath = {
+    PI: 3.14159,
+    square: function (num) {
+        return num * num;
+    },
+    cube: function (num) {
+        return num ** 3;
+    }
+}
