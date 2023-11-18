505ff37f00520c859640baf9ca186df0e0c9de14
Craig Hawkins <craighawki@gmail.com>
2023-11-17 17:25:19 -0800

    this

--- ThisKeyword/app.js
+++ ThisKeyword/app.js
@@ -1,9 +1,21 @@
 
-const cat = {
-    name: 'Blue Steele',
-    color: 'grey',
-    breed: 'scottish fold',
-    meow() {
-        console.log(this.color);
+// const cat = {
+//     name: 'Blue Steele',
+//     color: 'grey',
+//     breed: 'scottish fold',
+//     meow() {
+//         console.log("THIS IS:", this);
+//         console.log(`${this.name} says MEOWWW`);
+//     }
+// }
+
+// const meow2 = cat.meow;
+
+const hen = {
+    name: 'Helen',
+    eggCount: 0,
+    layAnEgg() {
+        this.eggCount += 1;
+        return 'EGG';
     }
 }
