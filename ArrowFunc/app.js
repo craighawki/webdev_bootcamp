710852afca7589691f2c9684d3ec368784986a50
craighawkins <craighawki@gmail.com>
2023-12-10 00:54:01 -0800

    arrow

--- ArrowFunc/app.js
+++ ArrowFunc/app.js
@@ -2,14 +2,55 @@
 //     return x + y;
 // }
 
-const add = (x, y) => {
-    return x + y;
-}
+// const add = (x, y) => {
+//     return x + y;
+// }
 
-const square = (x) => {
-    return x * x;
-}
+// const square = (x) => {
+//     return x * x;
+// }
 
-const rollDie = () => {
-    return Math.floor(Math.random() * 6) + 1;
-}
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6) + 1;
+// }
+
+// const rollDie = () => (
+//     Math.floor(Math.random() * 6) + 1
+// )
+
+// const greet = (name) => {
+//     return "Hey " + name;
+// }
+
+// // const add = (a, b) => (
+// //     a + b;
+// // )
+
+// const add = (a, b) => a + b;
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 80
+    },
+    {
+        title: 'The Stand',
+        score: 80
+    },
+    {
+        title: 'Dune',
+        score: 95
+    },
+    {
+        title: 'American Gigilo',
+        score: 80
+    },
+    {
+        title: 'Alien',
+        score: 82
+    }
+]
+
+const newMovies = movies.map(movie => (
+    `${movie.title} - ${movie.score / 10}`
+))
